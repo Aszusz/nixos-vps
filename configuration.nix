b50446875e72ba39afbbdbbd461b82b431aeb2dd
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2026-07-11 00:11:02 +0200

    Fix GitHub SSH host key

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@
 
   programs.ssh.knownHosts.github = {
     hostNames = [ "github.com" ];
-    publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqLZQoIUXnbuodrJVVt2Ah9QHSLSFGPtdkTnzyT2tG";
+    publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
   };
 
   systemd.services.pull-nixos-config = {
