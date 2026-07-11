56bae8fd94f0a6a560111394f35a58dc7a41ed2a
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2026-07-11 17:18:57 +0200

    Enable Podman service configuration

--- configuration.nix
+++ configuration.nix
@@ -41,6 +41,8 @@
 
   environment.systemPackages = [ pkgs.git pkgs.podman ];
 
+  virtualisation.podman.enable = true;
+
   programs.ssh.knownHosts.github = {
     hostNames = [ "github.com" ];
     publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
